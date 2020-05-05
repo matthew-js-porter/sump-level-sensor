272b2290ebd1921147062d6d4fd43226fb9d5e1e
Matt Porter <matthew.js.porter@gmail.com>
2020-05-05 15:23:51 -0400

    Adding setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+setup(
+    name='sump-level-sensor',
+    version='1.0',
+    description='A tool that will report if the water level in the sump is past a certain level using a float sensor',
+    author='Matt Porter',
+    author_email="matthew.js.porter@gmail.com",
+)
