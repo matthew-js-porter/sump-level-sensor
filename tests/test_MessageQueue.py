939bd78bae82a0a4a742e8fb1027dd79389f823f
Matt Porter <matthew.js.porter@gmail.com>
2020-05-06 13:28:59 -0400

    Adding MessageQueue.py for sending messages and entry point sump.py

---
+++ tests/test_MessageQueue.py
@@ -0,0 +1,16 @@
+import unittest
+from unittest.mock import Mock
+
+from sump.MessageQueue import MessageQueue
+
+
+class TestMessageQueue(unittest.TestCase):
+    def test_publish(self):
+        client = Mock()
+        message_queue = MessageQueue(client, 'topic')
+        message_queue.publish("message")
+        assert client.publish.called
+
+
+if __name__ == '__main__':
+    unittest.main()
