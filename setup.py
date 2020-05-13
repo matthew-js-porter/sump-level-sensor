52843d5189293adf7947db7b5baa28099b031075
Matt Porter <matthew.js.porter@gmail.com>
2020-05-13 07:41:42 -0400

    adding pytest with coverage

--- setup.py
+++ setup.py
@@ -5,9 +5,9 @@ setuptools.setup(
     description='A tool that will report if the water level in the sump is past a certain level using a float sensor',
     author='Matt Porter',
     author_email="matthew.js.porter@gmail.com",
-    setup_requires=['wheel', 'setuptools-lint'],
+    setup_requires=['wheel', 'setuptools-lint', 'pytest-runner'],
     install_requires=['gpiozero', 'boto3'],
-
+    tests_require=['pytest', 'pytest-cov', 'coverage'],
     packages=setuptools.find_packages(),
     test_suite='tests',
     entry_points = {
